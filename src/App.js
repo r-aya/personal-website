9a4f5e4b452668a396d41d8ff20863a1fe44b6bd
r-aya <rayajaima@gmail.com>
2022-08-09 15:45:10 -0400

    Temporarily removed navigation bar

--- src/App.js
+++ src/App.js
@@ -8,36 +8,25 @@ import Experience from "./pages/Experience";
 
 function App() {
   return (
-    // <div className="wrapper">
-    //   <div>
-    //     <IconButton ></IconButton>
-    //   </div>
-    //   {/* <div className="projectSection">
-    //     {/* <ProjectGrid></ProjectGrid>
+    <div className="wrapper">
+      <Router>
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: "calc(50% - 120px)",
+          }}
+        >
+          {/* <NavBar /> */}
+        </div>
 
-    //   </div> */}
-    // </div>
-    <>
-      <div className="wrapper">
-        <Router>
-          <div
-            style={{
-              position: "fixed",
-              top: 0,
-              left: "calc(50% - 120px)",
-            }}
-          >
-            <NavBar />
-          </div>
-
-          <Routes>
-            <Route exact path="/" element={<Landing />}></Route>
-            <Route path="/projects" element={<Projects />}></Route>
-            <Route path="/experience" element={<Experience />}></Route>
-          </Routes>
-        </Router>
-      </div>
-    </>
+        <Routes>
+          <Route exact path="/" element={<Landing />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/experience" element={<Experience />}></Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
